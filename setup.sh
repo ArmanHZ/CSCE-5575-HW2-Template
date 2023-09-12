afe3d0dc4871611c8df5cb19085098f07075e7ef
ArmanHZ <arman97@gmail.com>
2023-09-12 12:46:42 -0500

    Further setup.

--- setup.sh
+++ setup.sh
@@ -14,8 +14,13 @@ doesExist cast
 doesExist node
 doesExist pnpm
 
+printf "All the necessary tools are installed.\n\n"
+
 mkdir -p chain-end
 cd chain-end
 forge init . --no-commit
 cd ../front-end
 pnpm install
+
+printf "\npnpm start to start the react server.\nRun the helper functions from the 'front-end' directory.\nFor a list of the helper functions, check out the GitHub repository.\n\n"
+
