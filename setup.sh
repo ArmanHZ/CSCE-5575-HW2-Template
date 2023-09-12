8351fca88d2eff2929efa19a4f07aa08a60eb48a
ArmanHZ <arman97@gmail.com>
2023-09-12 12:27:49 -0500

    Setup script.

---
+++ setup.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+function doesExist() {
+	if ! command -v $1 > /dev/null; then
+		echo "$1 is not installed. Please install it before continueing."
+		exit 1
+	fi
+}
+
+doesExist curl
+doesExist git
+doesExist forge
+doesExist cast
+doesExist node
+doesExist pnpm
+
+cd chain-end
+forge init . --no-commit
+cd ../front-end
+pnpm install
