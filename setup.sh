899437714713625aaa919194be18c40116d0ab81
ArmanHZ <43811481+ArmanHZ@users.noreply.github.com>
2023-09-23 16:53:33 -0500

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,13 +1,15 @@
 #!/bin/bash
 
-if ! command -v $1 > /dev/null; then
-	if [[ $1 == 'forge' || $1 == 'cast' ]]; then
-		printf "\e[31;1m Foundry is not installed.\n Check https://book.getfoundry.sh/getting-started/installation for installation steps.\n\n\e[0m"
-	else
-		printf "\e[31;1m $1 is not installed. Please install it before continuing.\n\n\e[0m"
+function doesExist() {
+	if ! command -v $1 > /dev/null; then
+		if [[ $1 == 'forge' || $1 == 'cast' ]]; then
+			printf "\e[31;1m Foundry is not installed.\n Check https://book.getfoundry.sh/getting-started/installation for installation steps.\n\n\e[0m"
+		else
+			printf "\e[31;1m $1 is not installed. Please install it before continuing.\n\n\e[0m"
+		fi
+			exit 1
 	fi
-		exit 1
-fi
+}
 
 doesExist curl
 doesExist git
